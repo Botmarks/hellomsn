1faf42a48ab1b8464ceaa38a607ca28e00a35a8c
andy250 <akowal@outlook.com>
2016-09-09 22:50:45 +0200

    FIX: reponse from Messenger bot

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 const messenger = require('./lib/messenger');
 const logger = require('./lib/logger');
 
-/* --------- CONFIG ----------- */
+/* ---------- CONFIG ---------- */
 
 const VALIDATION_TOKEN = config.get('tokens.validationToken');
 if (!VALIDATION_TOKEN) {
@@ -23,7 +23,7 @@ if (!PAGE_ACCESS_TOKEN) {
   process.exit(1);
 }
 
-/* --------- WEB APP ------------ */
+/* ---------- WEB APP ---------- */
 
 let msn = new messenger({
   accessToken: PAGE_ACCESS_TOKEN
@@ -35,52 +35,56 @@ app.get('/', function (req, res) {
   res.send('This is the Botmarks HelloYou Messenger Bot (c) ' + new Date().getUTCFullYear());
 });
 
-app.get('/', function (req, res) {
+app.get('/webhook', function (req, res) {
+  let me = this;
   if (req.query['hub.mode'] === 'subscribe' &&
     req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    me._logger.debug('Validated webhook correctly');
+    logger.debug('Validated webhook correctly');
     res.status(200).send(req.query['hub.challenge']);
   } else {
-    me._logger.log('error', 'Webhook validation failed for token %s. Make sure the validation tokens match.', req.query['hub.verify_token']);
+    logger.log('error', 'Webhook validation failed for token %s. Make sure the validation tokens match.', req.query['hub.verify_token']);
     res.sendStatus(403);
   }
 });
 
-app.post('/', function (req, res) {
+app.post('/webhook', function (req, res) {
   var data = req.body;
   if (data.object == 'page') {
     data.entry.forEach(function (pageEntry) {
       pageEntry.messaging.forEach(function (messagingEvent) {
         logger.log('debug', 'Received messaging entry:\n\t%s', JSON.stringify(messagingEvent || {}));
+
+        // TODO: this cannot work with a regular Messenger bot, because MSN does not use the response. Instead MSN
+        // expects that the bot will send a POST to FB
         if (messagingEvent.message) {
-          receivedMessage(messagingEvent);
+          text = receivedMessage(messagingEvent);
         } else if (messagingEvent.postback) {
-          receivedPostback(messagingEvent);
+          text = receivedPostback(messagingEvent);
         } else {
           logger.log('warn', 'Webhook received strange/unsupported messagingEvent:\n\t%s', JSON.stringify(messagingEvent || {}));
         }
       });
     });
 
-    res.sendStatus(200);
+    res.status(200).send(text);
   }
 });
 
 function receivedMessage(event) {
   let me = this;
-  let text = 'hello you';
-  msn.sendTextMessage(event.sender.id, text)
+  let text = null;
+  if (event.message.text && event.message.text.toLowerCase().indexOf('my name is ') === 0) {
+    text = 'Hello ' + event.message.text.substr(11);
+  } else {
+    text = 'What is your name?'
+  }
+  return text;
+  // msn.sendTextMessage(event.sender.id, text)
 }
 
 // user tapped a button in structured message
 function receivedPostback(event) {
-  let me = this;
-  if (event.postback.payload) {
-    let text = 'hello you';
-    msn.sendTextMessage(event.sender.id, text)
-  } else {
-    logger.log('warn', 'Received postback without payload:\n\t%s', JSON.stringify(event || {}))
-  }
+  return 'Postback handled';
 }
 
 /* ---------- WEB SERVER ----------- */
